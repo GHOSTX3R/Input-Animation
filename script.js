100d77a0dab40f522b357025537e716125b831c4
EL YAZIDI Mohamed <elyazidi.edu@gmail.com>
2021-04-07 20:29:35 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Dom Stuff
+const fnameInput = document.querySelector('#fname');
+const lnameInput = document.querySelector('#lname');
+const emailInput = document.querySelector('#email');
+const password1Input = document.querySelector('#password1');
+const password2Input = document.querySelector('#password2');
+
+if (fnameInput.value === '') {
+}
+
+if (lnameInput.value === '') {
+}
+
+if (emailInput.value === '') {
+}
+
+if (password1Input.value === '') {
+}
+
+if (password2Input.value === '') {
+}
